3fa8e008d8e52ef031a19359becddbcb744d11ed
Rob Conde <Rob.Conde@ai-solutions.com>
2015-01-19 15:44:52 -0500

    - implemented workaround for the fact that WPF was using tiled rendering which caused the WPF version to have horrible performance as it would render the entire plot for each tile (24+ times) A fix that disables tiled rendering by default is forthcoming in Eto according to curtis.

--- Source/Examples/Eto/ExampleBrowser.Eto_NET40/Program.cs
+++ Source/Examples/Eto/ExampleBrowser.Eto_NET40/Program.cs
@@ -13,6 +13,14 @@ namespace ExampleBrowser.Eto_NET40
       [STAThread]
       static void Main(string[] args)
       {
+         if (Eto.Platform.Detect.IsWpf)
+         {
+            //Fix up fact that Eto drawable implementation is tiled by default
+            //This causes horrible performance because you render the entire plot
+            //for each tile
+            Eto.Style.Add<Eto.Wpf.Forms.Controls.DrawableHandler>(null, h => h.AllowTiling = false);
+         }
+
          new Application(Eto.Platform.Detect).Run(new MainForm());
       }
    }
